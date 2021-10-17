75483eb479ad413bc3628b7e3076690ba6cbb610
Saksham Raheja <sakshamraheja11@gmail.com>
2021-10-17 19:18:29 +0530

    Routing and ScrollToTop

--- src/App.js
+++ src/App.js
@@ -7,17 +7,21 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ProductsList from "./pages/ProductsList";
 import Product from "./pages/Product";
 import PageNotFound from "./pages/404";
+import Features from "./pages/Features";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <ScrollToTop />
         <Header />
         <Switch>
-          <Route exact path="/" component={Home}/>
-          <Route exact path="/contact" component={Contact}/>
-          <Route exact path="/products" component={ProductsList}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/products" component={ProductsList} />
           <Route path="/product/:id" component={Product} />
+          <Route path="/services" component={Features} />
           <Route component={PageNotFound} />
         </Switch>
         <Footer />
