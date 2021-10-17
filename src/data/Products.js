3b2e81225958b5e5d346a16377713e380df85512
Garv Jain <garvj03@gmail.com>
2021-10-17 17:54:48 +0530

    Updated List

--- src/data/Products.js
+++ src/data/Products.js
@@ -3,16 +3,16 @@ export const products = [
     id: 1,
     name: "Gaming Mouse",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, voluptatem!",
-    price: 55,
-    image: "black.jpeg",
+      "A powerful shock resistant gaming mouse",
+    price: 16,
+    image: "mouse.jpg",
   },
   {
     id: 2,
-    name: "Gaming Mouse",
+    name: "Gaming Keyboard",
     description:
-      "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim voluptatum suscipit doloribus repellat aperiam. Veniam hic illo itaque doloremque esse nam blanditiis, quaerat autem iure odit tenetur ipsum dolore facere delectus laudantium ipsam architecto eaque corrupti dolor labore unde consequuntur totam vel. Placeat maxime voluptate nam est maiores, quibusdam fugit, laboriosam eligendi architecto, optio ratione. Quae fugit provident deleniti, quidem ipsum fugiat voluptates perspiciatis libero delectus consequatur eum ea facilis illo temporibus omnis nihil neque saepe atque! Itaque, ratione quia quasi vero temporibus ex harum pariatur cupiditate veniam vitae, fugit voluptate quibusdam dolorem dicta quo ab. Consectetur rem voluptatibus repellendus?",
+      "A mechanical keyboard which is backlit by RGB lights for passionate gamers",
     price: 55,
-    image: "black.jpeg",
+    image: "keyboard.jpg",
   },
 ];
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,22 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 function PageNotFound() {
   return (
-    <div className="py-24 px-5">
-      <h1 className="text-2xl font-bold">404:) What you are looking for doesn't seem to exist.</h1>
+    <div className="px-5">
+      <div class="hero min-h-screen bg-base-200">
+        <div class="flex-col hero-content lg:flex-row-reverse">
+          <img
+            src="https://jstseguru.in/assets/images/jstseguru-404.gif"
+            class="max-w-sm rounded-lg"
+          />
+          <div>
+            <h1 class="mb-5 text-5xl font-bold">404</h1>
+            <p class="mb-5">What you are looking for doesn't seem to exist.</p>
+            <Link to="/"><button class="btn btn-primary">Go home</button></Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
