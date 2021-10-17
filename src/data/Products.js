b9430797830896fab812fcb27b9c3095c5dd9ac5
Saksham Raheja <sakshamraheja11@gmail.com>
2021-10-17 20:33:30 +0530

    Add Products

--- src/data/Products.js
+++ src/data/Products.js
@@ -2,8 +2,7 @@ export const products = [
   {
     id: 1,
     name: "Gaming Mouse",
-    description:
-      "A powerful shock resistant gaming mouse",
+    description: "A powerful shock resistant gaming mouse",
     price: 16,
     image: "mouse.jpg",
   },
@@ -14,5 +13,118 @@ export const products = [
       "A mechanical keyboard which is backlit by RGB lights for passionate gamers",
     price: 55,
     image: "keyboard.jpg",
+  },
+
+  {
+    id: 3,
+    name: "VR Headset",
+    description:
+      "Lightweight Headset — designed with innovative, breathable fabrics and injection foam molding",
+    price: 250,
+    image: "vr.jpeg",
+  },
+  {
+    id: 4,
+    name: "Gaming PC",
+    description:
+      "System: AMD Ryzen 5 3600 3.6GHz 6-Core | AMD B450 Chipset | 16GB DDR4 | 240GB SSD | 1TB HDD Graphics: NVIDIA GeForce GTX 2060 6GB Video Card | 1x HDMI | 1x DisplayPort | 1x Dvi Port",
+    price: 900,
+    image: "pc.jpg",
+  },
+  {
+    id: 5,
+    name: "Gaming Laptop",
+    description:
+      "38.1 cm (15) gaming laptop with the latest overclocked Intel® 6-Core™ CPU and upgraded cooling technology.",
+    price: 1100,
+    image: "laptop.jpeg",
+  },
+  {
+    id: 6,
+    name: "Gaming Monitor",
+    description: "24” FHD FreeSync 144Hz 1ms Height Adjustable Gaming Monitor",
+    price: 219,
+    image: "monitor.jpeg",
+  },
+
+  {
+    id: 7,
+    name: "Gaming Chair",
+    description:
+      "Gaming Chair with Foot Rest & Computer Chair -Black&Red Adjustable PU Leather & Ergonomic Chair with Lumbar Support, Reclining, Easy Assemble, Comfort Gaming Chair That Support Upto 120kg",
+    price: 129,
+    image: "chair.jpeg",
+  },
+  {
+    id: 8,
+    name: "Advanced Gaming Chair",
+    description:
+      "Using Luxury feeling executive chair - very comfortable texture and superior quality with Massage function",
+    price: 429,
+    image: "chair2.jpeg",
+  },
+  {
+    id: 9,
+    name: "Gaming Mouse",
+    description: "A powerful shock resistant gaming mouse",
+    price: 16,
+    image: "mouse.jpg",
+  },
+  {
+    id: 10,
+    name: "Gaming Keyboard",
+    description:
+      "A mechanical keyboard which is backlit by RGB lights for passionate gamers",
+    price: 55,
+    image: "keyboard.jpg",
+  },
+
+  {
+    id: 11,
+    name: "VR Headset",
+    description:
+      "Lightweight Headset — designed with innovative, breathable fabrics and injection foam molding",
+    price: 250,
+    image: "vr.jpeg",
+  },
+  {
+    id: 12,
+    name: "Gaming PC",
+    description:
+      "System: AMD Ryzen 5 3600 3.6GHz 6-Core | AMD B450 Chipset | 16GB DDR4 | 240GB SSD | 1TB HDD Graphics: NVIDIA GeForce GTX 2060 6GB Video Card | 1x HDMI | 1x DisplayPort | 1x Dvi Port",
+    price: 900,
+    image: "pc.jpg",
+  },
+  {
+    id: 13,
+    name: "Gaming Laptop",
+    description:
+      "38.1 cm (15) gaming laptop with the latest overclocked Intel® 6-Core™ CPU and upgraded cooling technology.",
+    price: 1100,
+    image: "laptop.jpeg",
+  },
+  {
+    id: 14,
+    name: "Gaming Monitor",
+    description: "24” FHD FreeSync 144Hz 1ms Height Adjustable Gaming Monitor",
+    price: 219,
+    image: "monitor.jpeg",
+  },
+
+  {
+    id: 15,
+    name: "Gaming Chair",
+    description:
+      "Gaming Chair with Foot Rest & Computer Chair -Black&Red Adjustable PU Leather & Ergonomic Chair with Lumbar Support, Reclining, Easy Assemble, Comfort Gaming Chair That Support Upto 120kg",
+    price: 129,
+    image: "chair.jpeg",
+  },
+  {
+    id: 16,
+    name: "Advanced Gaming Chair",
+    description:
+      "Using Luxury feeling executive chair - very comfortable texture and superior quality with Massage function",
+    price: 429,
+    image: "chair2.jpeg",
   },
 ];
